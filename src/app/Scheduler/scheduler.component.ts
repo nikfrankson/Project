9510d0be81c9f865a929f07a2bda8408d4ceb0b4
bpetec <bpetec@yahoo.com>
2019-11-24 21:49:54 -0800

    Added header/nav to scheduler

--- src/app/Scheduler/scheduler.component.ts
+++ src/app/Scheduler/scheduler.component.ts
@@ -2,12 +2,15 @@ import { Component } from '@angular/core';
 import { EventSettingsModel, DayService, WeekService, WorkWeekService,
   MonthService, AgendaService } from '@syncfusion/ej2-angular-schedule';
 // NOTE: Run npm i @syncfusion/ej2-angular-schedule
+import { LocalStorageService } from '../localStorageService';
+
 @Component({
     selector: 'app-scheduler',
     providers: [DayService, WeekService, WorkWeekService, MonthService, AgendaService],
     // specifies the template string for the Schedule component
-    template: `<ejs-schedule width='100%' height='550px' [selectedDate]="selectedDate"
-  [eventSettings]="eventSettings" > </ejs-schedule>`
+    template: `<ejs-schedule width='100%' height='85%' [selectedDate]="selectedDate"
+  [eventSettings]="eventSettings" > </ejs-schedule>`,
+  templateUrl: './scheduler.component.html'
 })
 export class SchedulerComponent {
     public data: object[] = [{
